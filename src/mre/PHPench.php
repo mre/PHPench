9a790f9675261635d4e350608672c80bb8de26dc
Markus Poerschke <markus.poerschke@trivago.com>
2014-10-30 16:48:26 +0100

    Support comparing different implementations #5

--- src/mre/PHPench.php
+++ src/mre/PHPench.php
@@ -7,17 +7,23 @@ use PHP_Timer;
 
 class PHPench
 {
-  public function __construct() {
+  private $plotIndex = -1;
+
+  public function __construct($title = 'untitled') {
     $this->plot = new GnuPlot;
     $this->plot->reset();
-    $this->plot->setGraphTitle('PHPench benchmark');
+    $this->plot->setGraphTitle($title);
     #$this->plot->setTitle(0, 'The moving sinus');
     #$this->plot->setTitle(1, 'The moving cosinus');
   }
 
-  public function plot($benchFunction, $benchRange, $keepAlive=False) {
+  public function plot($benchFunction, $benchRange, $title, $keepAlive=False) {
+    $this->plotIndex++;
+
+    $this->plot->setTitle($this->plotIndex, $title);
+
     foreach($benchRange as $i) {
-      $this->bench($benchFunction, $i);
+      $this->bench($benchFunction, $i, $this->plotIndex);
     }
 
     if ($keepAlive) {
@@ -33,11 +39,11 @@ class PHPench
                ->writePng($filename);
   }
 
-  protected function bench($benchFunction, $i) {
+  protected function bench($benchFunction, $i, $plotIndex) {
     PHP_Timer::start();
     $benchFunction($i);
     $time = PHP_Timer::stop();
-    $this->plot->push($i, $time);
+    $this->plot->push($i, $time, $plotIndex);
     $this->plot->refresh();
   }
 }
